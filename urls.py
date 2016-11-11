8e8ee722ba79419bcdbb90c349e5e6753901deee
Ilya Khramtsov <minigun@protonmail.com>
2016-11-11 14:14:00 +0500

    edit

--- urls.py
+++ urls.py
@@ -1,12 +1,7 @@
 from django.conf.urls import url
-from django.contrib import admin
-from django.conf.urls.static import static
-from django.conf import settings
-from likes import *
+from .likes import *
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', index),
-    url(r'^like', uri_manager, name='likes'),
-    url(r'^auth', authorize, name='auth'),
+	url(r'^like', uri_manager, name='likes'),
+	url(r'^auth', authorize, name='auth'),
 ]
